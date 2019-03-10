53fab6a174f61269de90521a93a0698d19e9119d
Tower0314 <393175162@qq.com>
2019-03-10 22:10:00 +0800

    Description：turtle绘图九角星，做了一个头像 auth：taowei date：2019-03-10

---
+++ Nine-pointed star.py
@@ -0,0 +1,15 @@
+from turtle import *
+pencolor('green')
+pensize(1)
+speed(0)
+for i in range(60):
+	for i in range(9):
+		fd(300)
+		right(160)
+	left(1)
+right(30)
+penup()
+fd(2000)
+#circle(180,360)
+input()
+
